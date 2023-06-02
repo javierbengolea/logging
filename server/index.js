d5328554c26e58cc1557e2b2adebc1ffe063ee40
Javier Bengolea <javierbengolea@yahoo.com.ar>
2023-06-02 09:51:37 -0300

    ---

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+const PORT = process.env.PORT || 3060;
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
